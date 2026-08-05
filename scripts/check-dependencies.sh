f2c81aabe3939f695d7126c2b7d5fd1380617374
adamtc007 <adamtc007@gmail.com>
2026-08-05 16:11:45 +0100

    ci: align release guards with pack policy

--- scripts/check-dependencies.sh
+++ scripts/check-dependencies.sh
@@ -66,7 +66,7 @@ assert_allowed_workspace_dependencies semantic-embedder
 assert_allowed_workspace_dependencies dsl-core dsl_types
 assert_allowed_workspace_dependencies sem_os_core dsl-core dsl_types sem_os_types
 assert_allowed_workspace_dependencies sem_os_ontology dsl_types sem_os_types semantic-decision-contracts
-assert_allowed_workspace_dependencies sem_os_policy dsl_types sem_os_core sem_os_ontology sem_os_types semantic-decision-contracts
+assert_allowed_workspace_dependencies sem_os_policy dsl_types sem_os_core sem_os_ontology sem_os_types semantic-decision-contracts semantic-pack
 assert_no_host_sources
 
 if [ "$fail" -ne 0 ]; then
